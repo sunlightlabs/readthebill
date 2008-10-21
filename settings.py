c50e5627cf6cd115423954e62b7bc1b086fa588c
jcarbaugh <jcarbaugh@f148783c-4789-4d66-b4b6-0f25ae973971>
2008-10-21 19:06:29 +0000

    added gatekeeper dependency
    git-svn-id: https://source.sunlightlabs.com/readthebill/trunk@13 f148783c-4789-4d66-b4b6-0f25ae973971

--- settings.py
+++ settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'gatekeeper',
     'feedinator',
     'sunlightcore',
     'readthebill.rtb',
