18cda1d6752ea20dcc1059a97b081fdc316ec7bc
jcarbaugh <jcarbaugh@f148783c-4789-4d66-b4b6-0f25ae973971>
2009-02-23 12:56:48 +0000

    make signup form work on all pages
    git-svn-id: https://source.sunlightlabs.com/readthebill/trunk@69 f148783c-4789-4d66-b4b6-0f25ae973971

--- rtb/templatetags/rtb.py
+++ rtb/templatetags/rtb.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.loader import render_to_string
+from readthebill.rtb.forms import SignupForm
 from readthebill.rtb.models import Organization
 import random
 
@@ -10,3 +11,8 @@ def random_orgs(count):
     orgs = Organization.objects.filter(logo__isnull=False)
     orgs = random.sample(orgs, count)
     return render_to_string("rtb/random_orgs.html", {"orgs": orgs})
+
+@register.simple_tag
+def signup_form():
+    form = SignupForm()
+    return form.as_ul()
