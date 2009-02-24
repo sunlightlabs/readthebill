4569086de6a9cd16de0737b08d52feb4b577d541
jcarbaugh <jcarbaugh@f148783c-4789-4d66-b4b6-0f25ae973971>
2009-02-24 03:03:08 +0000

    dbinit with blogs to aggregate
    git-svn-id: https://source.sunlightlabs.com/readthebill/trunk@76 f148783c-4789-4d66-b4b6-0f25ae973971

--- dbinit.sh
+++ dbinit.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 ./manage.py addfeed http://www.sunlightfoundation.com/feeds/latest/ sunlightfoundation
+./manage.py addfeed http://www.thenextright.com/fbfeed thenextright
+./manage.py addfeed http://www.cnewmark.com/rss.xml craignewmark
+./manage.py addfeed http://pogoblog.typepad.com/pogo/index.rdf pogo
+./manage.py addfeed http://personaldemocracy.com/node/feed pdf
+./manage.py addfeed http://www.ombwatch.org/blog/all/all/all/feed ombwatch
+./manage.py addfeed http://www.mediaaccess.org/rss/ mediaaccess
+./manage.py addfeed http://www.fas.org/blog/secrecy/feed secrecynews
+./manage.py addfeed http://www.eff.org/rss/updates.xml eff
+./manage.py addfeed http://www.opensecrets.org/news/rss.xml crp
+./manage.py addfeed http://blog.cdt.org/feed/ cdt
+
 ./manage.py addfeed "http://dev.opencongress.org/bill/readthebill.rss?sort=gpo&show_resolutions=false" rushedbills
 ./manage.py addfeed "http://feeds.delicious.com/v2/rss/sunlight_foundation/bundle:readthebill?count=15" press
+
 ./manage.py updatefeeds
 
 ./manage.py loaddata data/orgs.json
